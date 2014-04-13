5d51d41cf1e0ea7cae1387574d57b0ff0f017dc8
beefchimi <curtdulmage@gmail.com>
2014-04-13 15:14:33 -0400

    adding files for testing purposes only

---
+++ bradio/temp/index.php
@@ -0,0 +1,27 @@
+<?php
+
+	$header  = "../../assets/inc/header.php";
+	$footer  = "../../assets/inc/footer.php";
+
+	include_once($header);
+
+?>
+
+	<main role="main">
+
+		<section>
+
+			<article data-container="width-full" class="content-box">
+
+				<h1>TEST</h1>
+				<p>This page is a placeholder for the bRadio component.</p>
+
+				<a href="#" id="alert-test" data-control="alert" title="Trigger Alert">Trigger Notification Alert</a>
+
+			</article>
+
+		</section>
+
+	</main>
+
+<?php include_once($footer); ?>
